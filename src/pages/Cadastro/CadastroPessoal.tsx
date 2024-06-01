3e655f4d69e192dc9a0513069e87fa266799ac91
Jrm1992 <marquesjoseroberto@proton.me>
2024-06-01 18:01:18 -0300

    aula 02

--- src/pages/Cadastro/CadastroPessoal.tsx
+++ src/pages/Cadastro/CadastroPessoal.tsx
@@ -1,34 +1,45 @@
+import { Controller, useForm } from "react-hook-form";
 import {
   Button,
-  Label,
+  ErrorMessage,
   Fieldset,
+  Form,
   Input,
   InputMask,
-  Form,
+  Label,
   Titulo,
-  ErrorMessage,
 } from "../../components";
-import { useForm, Controller } from "react-hook-form";
-import { useEffect } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormInputTipos {
-  nome: string;
-  email: string;
-  telefone: string;
-  senha: string;
-  senhaVerificada: string;
+const esquemaCadastro = z.object({
+  nome: z.string().min(5, "O nome deve ter pelo menos 5 letras").transform((v) => capitalizeWords(v)),
+  email: z.string().email("Email inválido").transform((v) => v.toLowerCase()),
+  telefone: z.string().min(1, "O telefone é obrigatório"),
+  senha: z.string().min(6, "A senha deve ter pelo menos 6 letras"),
+  senhaVerificada: z.string().min(1, "Confirme sua senha"),
+}).refine(({ senha, senhaVerificada }) => senha === senhaVerificada, {
+  message: "As senhas devem ser iguais",
+  path: ["senhaVerificada"],
+})
+
+function capitalizeWords(sentence: string) {
+  return sentence.split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  }).join(' ');
 }
+
+type FormInputTipos = z.infer<typeof esquemaCadastro>;
 
 const CadastroPessoal = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitSuccessful },
-    watch,
+    formState: { errors },
     control,
-    reset,
   } = useForm<FormInputTipos>({
     mode: "all",
+    resolver: zodResolver(esquemaCadastro),
     defaultValues: {
       nome: "",
       email: "",
@@ -38,32 +49,9 @@ const CadastroPessoal = () => {
     },
   });
 
-  useEffect(() => {
-    reset();
-  }, [reset, isSubmitSuccessful]);
-
   const aoSubmeter = (dados: FormInputTipos) => {
     console.log(dados);
   };
-
-  const senha = watch("senha");
-
-  const validaSenha = {
-    obrigatorio: (val: string) =>
-      !!val || "Por favor, insira a senha novamente",
-    tamanhoMinimo: (val: string) =>
-      val.length >= 6 || "A senha deve ter pelo menos 6 caracteres",
-    senhaIguais: (val: string) => val === senha || "As senhas não correspondem",
-  };
-
-  function validarEmail(valor: string) {
-    const formatoEmail = /^[^\s@]+@alura\.com\.br$/;
-    if (!formatoEmail.test(valor)) {
-      console.error("Endereço de email é inválido para este domínio");
-      return false;
-    }
-    return true;
-  }
 
   return (
     <>
@@ -93,10 +81,7 @@ const CadastroPessoal = () => {
             placeholder="Insira seu endereço de email"
             type="email"
             $error={!!errors.email}
-            {...register("email", {
-              required: "O campo de email é obrigatório",
-              validate: validarEmail,
-            })}
+            {...register("email")}
           />
           {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </Fieldset>
@@ -133,13 +118,7 @@ const CadastroPessoal = () => {
             placeholder="Crie uma senha"
             type="password"
             $error={!!errors.senha}
-            {...register("senha", {
-              required: "O campo de senha é obrigatório",
-              minLength: {
-                value: 6,
-                message: "A senha deve ter pelo menos seis caracteres",
-              },
-            })}
+            {...register("senha")}
           />
           {errors.senha && <ErrorMessage>{errors.senha.message}</ErrorMessage>}
         </Fieldset>
@@ -150,10 +129,7 @@ const CadastroPessoal = () => {
             placeholder="Repita a senha anterior"
             type="password"
             $error={!!errors.senhaVerificada}
-            {...register("senhaVerificada", {
-              required: "Repita a senha",
-              validate: validaSenha,
-            })}
+            {...register("senhaVerificada")}
           />
           {errors.senhaVerificada && (
             <ErrorMessage>{errors.senhaVerificada.message}</ErrorMessage>
